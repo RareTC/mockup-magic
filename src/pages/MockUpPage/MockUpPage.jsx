f6bbe58447cee8ba0b82fe66f079deffeb791d72
RareTC <trevorjcampbell@aol.com>
2023-03-29 10:28:39 -0700

    Implement select mock from dropdown

--- src/pages/MockUpPage/MockUpPage.jsx
+++ src/pages/MockUpPage/MockUpPage.jsx
@@ -2,20 +2,26 @@ import './MockUpPage.css';
 import { useState } from 'react';
 import PaletteForm from '../../components/PaletteForm/PaletteForm';
 import UIBasic from '../../mocks/UIBasic/UIBasic';
+import UIBasic2 from '../../mocks/UIBasic2/UIBasic2';
 
 export default function MockUpPage() {
+  const [activePalette, setActivePalette] = useState(null);
+  
+  const mocks = [
+    { title: 'Basic UI', ui: activePalette && <UIBasic palette={activePalette} /> },
+    { title: 'Second UI', ui: activePalette && <UIBasic2 palette={activePalette} /> },
+  ];
 
-    const [activePalette, setActivePalette] = useState(null);
+  const [activeMock, setActiveMock] = useState(mocks[0].title);
 
-    const mocks = [
-      
-      activePalette && <UIBasic palette={activePalette} />
-    ];
 
-    return (
-      <>
-        <PaletteForm  setActivePalette={setActivePalette} />
-        { mocks[0] }
-      </>
+  return (
+    <>
+      <PaletteForm setActivePalette={setActivePalette} />
+      {mocks.find(m => m.title === activeMock).ui}
+      <select value={activeMock} onChange={(evt) => setActiveMock(evt.target.value)}>
+        {mocks.map(m => <option value={m.title}>{m.title}</option>)}
+      </select>
+    </>
   )
 }
