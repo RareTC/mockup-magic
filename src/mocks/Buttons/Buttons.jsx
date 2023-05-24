a8b71042ce212872247d44604d2b165e0871ab78
RareTC <trevorjcampbell@aol.com>
2023-05-24 14:58:50 -0700

    Nav cursor

--- src/mocks/Buttons/Buttons.jsx
+++ src/mocks/Buttons/Buttons.jsx
@@ -7,11 +7,11 @@ export default function Buttons({ palette }) {
     return (
         <>
             <main className="UIBasic-main">
-                <button class="UIbutton" style={{background: c0, color: c1}}>Button 1</button>
-                <button class="UIbutton" style={{background: c1, color: c2}}>Button 2</button>
-                <button class="UIbutton" style={{background: c2, color: c3}}>Button 3</button>
-                <button class="UIbutton" style={{background: c3, color: c4}}>Button 4</button>
-                <button class="UIbutton" style={{background: c4, color: c0}}>Button 5</button>
+                <button class="UIbutton" style={{background: c0, color: c2}}>Button 1</button>
+                <button class="UIbutton" style={{background: c1, color: c4}}>Button 2</button>
+                <button class="UIbutton" style={{background: c2, color: c0}}>Button 3</button>
+                <button class="UIbutton" style={{background: c3, color: c2}}>Button 4</button>
+                <button class="UIbutton" style={{background: c4, color: c2}}>Button 5</button>
             </main>
         </>
     );
